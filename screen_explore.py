a0586487738960d04e596379e7fa6fc98bb140e6
Christian Woll <christianwoll@Christian-Woll-MacBook-Pro.local>
2017-09-25 11:21:55 -0700

    fun begins

--- screen_explore.py
+++ screen_explore.py
@@ -3,8 +3,6 @@ import random
 import numpy as np
 from PIL import Image
 from mss import mss
-import pytesseract
-from sklearn.cluster import KMeans
 from tile_encoder import LowerTileEncoder, UpperTileEncoder
 
 
@@ -12,61 +10,43 @@ from tile_encoder import LowerTileEncoder, UpperTileEncoder
 
 sct = mss()
 
-# BUG:
-# The dict is modified in place by mss so make a copy for each call.
-mon = {'top': 0, 'left': 0, 'width': 1280, 'height': 800}
-
-def grab_tiles(num_tiles, tile_size=(64,64)):
-    sct_img = sct.grab(dict(mon))
+def grab_tile(x, y, width=64, height=64):
+    sct_img = sct.grab({'top': y, 'left': x, 'width': width, 'height': height})
 
     print('<screenshot taken>')
 
     pixels = np.array(sct_img)
     grayscale = np.dot(pixels[...,:3], [0.299, 0.587, 0.114])
 
-    tiles = []
-    for _ in range(num_tiles):
-        i = random.randrange(grayscale.shape[0] - tile_size[0])
-        j = random.randrange(grayscale.shape[1] - tile_size[1])
-        tile = grayscale[i:i+tile_size[0],j:j+tile_size[1]]
-        tiles.append(tile)
-
-    return tiles
+    return grayscale
 
 
+
+tile_size = 64
+x, y = 100, 100
+tiles = [grab_tile(x, y)]*64
 
 upperTileEncoder = UpperTileEncoder()
 lowerTileEncoder = LowerTileEncoder()
 
-epoch = 1;tiles = []
 while True:
-    print('Beginning epoch ' + str(epoch))
+    x = x + random.randrange(5) - 2
+    y = y + random.randrange(5) - 2
 
-    tiles = [img for img in tiles if random.random() > 1.0 / 100]
-    tiles += grab_tiles(100)
+    x = max(0, min(1216, x))
+    y = max(0, min(736, y))
 
-    print(str(len(tiles)) + ' tiles in corpus.')
+    print(str(len(tiles)) + ' tiles.')
+    print('@ ' + str(x) + ', ' + str(y))
 
-    print('Training upper tile encoder...')
-    upperTileEncoder.fit(tiles)
-    upperTileEncoder.save()
+    tiles.append(grab_tile(x, y))
+    tiles = tiles[-10000:]
 
-    print('Training lower tile encoder...')
-    chips = upperTileEncoder.encode(tiles)
-    lowerTileEncoder.fit(chips)
-    lowerTileEncoder.save()
-
-    num_clusters = 10
-    points = lowerTileEncoder.encode(chips)
-    kmeans = KMeans(n_clusters=num_clusters).fit(points)
-
-    clusters = [[] for _ in range(num_clusters)]
-    for idx, label in enumerate(kmeans.labels_):
-        clusters[label].append(tiles[idx])
-
-    for cluster in clusters: print(len(cluster))
-
-
-    epoch += 1
-
-    print(' ')
+    if len(tiles) > 1000:
+        sample = random.sample(tiles, 1000)
+        upperTileEncoder.fit(sample)
+        upperTileEncoder.save()
+        
+        chips = upperTileEncoder.encode(sample)
+        lowerTileEncoder.fit(chips)
+        lowerTileEncoder.save()
